a8e6a59c5ab361c82adb53e78d094068975210b8
ashe <karlokuhada3@gmail.com>
2025-06-14 12:12:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    genius = lyricsgenius.Genius("LfRP3xS3m-0nF7XX_6UleJPWQxRA6_mv1XW55MnnS4AKZBqp2nwQp6gOnn_zi_GA")
-    #song = genius.search_song("Money Trees", "Kendrick Lamar", get_full_info=False)
-    #if genius != None:
-        #title = song.title
-        #artist = song.artist
-        #lyrics = re.sub(r'^.*?(\[.*)', r'\1', song.lyrics, flags=re.DOTALL)
-    #else:
-        #title = "not found"
-        #artist = "not found"
-        #lyrics = "not found"
-    #return(render_template("index.html", title=title, artist=artist, lyrics=lyrics))
     return(render_template("index.html"))
 
 if __name__ == '__main__':
